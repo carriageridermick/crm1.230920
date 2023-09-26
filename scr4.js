8fc3f861187acb22123a274717756b332bbb7066
carriageridermick <131271401+carriageridermick@users.noreply.github.com>
2023-09-26 14:48:44 +0300

    Delete scr4.js

--- scr4.js
+++
@@ -1,6 +0,0 @@
-let uInp = prompt(`Please enter a number, either a positive or negative one:`) //userInput
-if (uInp < 0) {
-    alert("You've entered a Negative number! Nicely done.")
-} else {
-    alert("You've entered a Positive number! Not bad!")
-}
