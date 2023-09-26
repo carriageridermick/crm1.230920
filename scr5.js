533a666d0eeadd114bafeccbd046f8a774bf4b91
carriageridermick <131271401+carriageridermick@users.noreply.github.com>
2023-09-26 14:48:29 +0300

    Delete scr5.js

--- scr5.js
+++
@@ -1,8 +0,0 @@
-async function apiFetch(url) {
-    const response = await fetch("https://api.meteo.lt/v1/places/kaunas/forecasts/long-term");
-    const final = await response.json();
-    console.log(final.json);
-} //funkcija kurios kodas paaiškina ją pačią
-
-let duoMeteo = apiFetch(); //duomenysMeteo
-console.log(duoMeteo);
